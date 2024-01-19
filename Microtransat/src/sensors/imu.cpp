e1855bd41227ca4ad497c0d1dc638191c45e9332
Mitchell Turton <mbturton33@gmail.com>
2024-01-18 23:19:42 -0500

    Implemented IMU driver

--- Microtransat/src/sensors/imu.cpp
+++ Microtransat/src/sensors/imu.cpp
@@ -4,5 +4,33 @@
 #include <Wire.h>
 #include <Adafruit_BNO055.h>
 #include <Adafruit_Sensor.h>
-#include <utility/imumaths.h>
 
+#define BNO55_SENSOR_ID 55
+
+Adafruit_BNO055 bno = Adafruit_BNO055(BNO55_SENSOR_ID);
+
+
+void imu_driver_open() {
+    if (!bno.begin()) {
+        Serial.println("No BNO055 detected...");
+        // Log_Message("No BNO055 detected...", ERROR);
+    }
+
+    delay(1000);
+
+    bno.setExtCrystalUse(true);
+}
+
+void imu_driver_close() { }
+
+IMU_DATA* imu_driver_read() {
+    IMU_DATA* imu_data = (IMU_DATA*) (sizeof(IMU_DATA));
+
+    imu_data->accelerometer = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+    imu_data->gyroscope = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+    imu_data->linear_acceleration = bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+    imu_data->gravity = bno.getVector(Adafruit_BNO055::VECTOR_GRAVITY);
+    imu_data->quaternion = bno.getQuat();
+
+    return imu_data;
+}
